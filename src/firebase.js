d66fe9ce092b8b7c6662047248a4919d2b0670b2
Giusseppe PC <giudelvecchio@gmail.com>
2020-08-19 12:27:32 -0400

    Replaced config with environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase'
 
 const config = {
-    apiKey: "AIzaSyB-Pb37Xh3g6MPskvVFr0rAzB4njC0CLTQ",
-    authDomain: "brian-blog-432d0.firebaseapp.com",
-    databaseURL: "https://brian-blog-432d0.firebaseio.com",
-    projectId: "brian-blog-432d0",
-    storageBucket: "brian-blog-432d0.appspot.com",
-    messagingSenderId: "932167429311",
-    appId: "1:932167429311:web:4863d5f0493f06d9b2aa0a",
-    measurementId: "G-Y52PC8F65D"
+    apiKey: process.env.REACT_APP_FIREBASE_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 // Initialize Firebase
 firebase.initializeApp(config);
