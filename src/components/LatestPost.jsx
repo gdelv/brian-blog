d7bc83f549ad765e31078506a34835602a683e23
Giusseppe Del Vecchio <giudelvecchio@gmail.com>
2020-09-28 01:57:12 -0400

    blog page complete

--- src/components/LatestPost.jsx
+++ src/components/LatestPost.jsx
@@ -1,33 +1,31 @@
-import React, { useState } from 'react'
-import { Title } from '../components/layout'
+import React from 'react'
+import { Title, Subtitle } from '../components/layout'
 
 export default function LatestPost(props) {
-    const [translate, setTranslate] = useState('-170')
 
     console.log(props);
     return (
         <>
-            <Title title='Newest Post' size={2} className=''/>
-            <article class="b-promotion tile is-child box">
+            <Title title='Newest Post' size={5} className=''/>
+            <article className="b-promotion tile is-child box is-light">
             <a href="/blog/estimate-arv">
-                <img alt="red haired caucasian woman comparing apples and oranges." title="The Ultimate Guide to Quickly Estimating a Property’s ARV (After Repair Value)" class="b-img-fluid rounded b-promotion__img" src="https://assets2.biggerpockets.com/uploads/wordpress_blog_post/image/7678/homepage_featured_comparing-apples-to-oranges.jpg"/>
+                <img alt="red haired caucasian woman comparing apples and oranges." title="The Ultimate Guide to Quickly Estimating a Property’s ARV (After Repair Value)" className="b-img-fluid rounded b-promotion__img" style={{minWidth: '100%'}} src="https://assets2.biggerpockets.com/uploads/wordpress_blog_post/image/7678/homepage_featured_comparing-apples-to-oranges.jpg"/>
             </a>
-            <div class="b-content__info" style={{bottom: `${translate}px`}} onMouseLeave={() => setTranslate('-170')} onMouseEnter={() => setTranslate('0')}>
-                <p>
-                    <span class="b-badge"><a href="/blog/category/real-estate-deal-analysis-advice">Real Estate Deal Analysis &amp; Advice</a></span>
-                    <time class="b-date" datetime="2020-09-24 14:00:26 -0700" itemprop="datePublished">Sep 24, 2020</time>
-                </p>
-                <div class="b-promotion__blog-hero">
-                    <h3 itemprop="name headline"><a class="b-blog-link" href="/blog/estimate-arv">The Ultimate Guide to Quickly Estimating a Property’s ARV (After Repair Value)</a></h3>
-                    <div class="b-author" itemprop="author" itemscope="" itemtype="http://schema.org/Person">
-                        <div class="b-author__img">
-                            <img class="rounded-circle" src="https://assets0.biggerpockets.com/uploads/wordpress_author/avatar/10/tiny_1565904223-avatar-clemsoninvestor.jpg" alt='random'/>
-                        </div>
-                        <div class="b-author__name" itemprop="name">
-                            <a href="/blog/contributors/chadcarson">By Chad Carson</a>
-                        </div>
+            <div className="b-content__info">
+            <div className='is-flex' style={{justifyContent: 'space-between'}}>
+                <Subtitle title={props.post.category} style={{fontSize: '.8rem', padding: '.25em'}}/>
+                <Subtitle title={new Date(props.post.datePosted).toDateString()} style={{fontSize: '.8rem'}}/>
+            </div>
+                <div className="b-promotion__blog-hero">
+                    <h3 itemprop="name headline" style={{fontSize:'1.1em'}} ><a className="b-blog-link" href="/blog/estimate-arv">{props.post.title}</a></h3>
+                    <div className='is-flex' style={{alignItems: 'center', justifyContent:'flex-start', paddingTop: '.5em'}}>
+                        <figure class="image is-48x48">
+                            <img src={props.post.authorImg} alt="Placeholder" className='is-rounded'/>
+                        </figure>
+                        <Subtitle title={props.post.author} className='pl-2'/>
                     </div>
-                    <p class="b-content__short">What’s the #1 skill you need to have to make money off of real estate? I would argue it’s learning how to accurately value a property. This guide will explain, step by step, how to do a quick and dirty estimate of value for single family or other small residential property.
+                    <p className="b-content__short">
+                        {props.post.body}
                     </p>
                 </div>
             </div>
--- src/screens/Posts.jsx
+++ src/screens/Posts.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import firebase from '../firebase'
-import './Posts.css'
+// import './Posts.css'
 import LatestPost from '../components/LatestPost'
 import { Subtitle, Title } from '../components/layout'
 // import PopularPosts from '../components/PopularPosts';
@@ -21,8 +21,8 @@ let popularArr = [];
 const renderPosts = () => {
     for (let i = 1; i < 4; i++) {
         const element = posts[i];
-        popularArr.push(element)
-        
+        popularArr.push(element);
+        console.log(popularArr);
     }
     if (posts.length) {
         return (
@@ -30,44 +30,40 @@ const renderPosts = () => {
                 <div class="tile is-vertical">
                     <div class="tile">
                     <div class="tile is-parent is-vertical is-7">
-                        <LatestPost/>
+                        <LatestPost post={posts[0]}/>
                     </div>
                     <div class="tile is-parent is-vertical">
-                        <Title title='Popular Posts' size={2} />
+                        <Title title='Popular Posts' size={5} />
                         {/* START OF MAP COMPONENT */}
-                        <article class="tile is-child box notification is-primary">
-                            <div className="columns is-mobile">
-                                <div className="column is-4 has-text-centered">   
-                                    <figure class="image is-128x128">
-                                        <img src="https://bulma.io/images/placeholders/128x128.png" alt='random' />
-                                    </figure>
-                                </div>
-                                <div className="column">
-                                    <div className='is-flex' style={{justifyContent: 'space-between'}}>
-                                        <Subtitle title='Real Estate Investing Basics' style={{fontSize: '.8rem', background: 'blue', padding: '.25em'}}/>
-                                        <Subtitle title='Sep 23, 2020' style={{fontSize: '.8rem'}}/>
+                            {popularArr.map(post => {
+                                return (
+                                    <article class="tile is-child box notification is-light">
+                                    <div className="columns is-mobile">
+                                        <div className="column is-4 has-text-centered">   
+                                            <figure class="image is-128x128">
+                                                <img src="https://bulma.io/images/placeholders/128x128.png" alt='random' />
+                                            </figure>
+                                        </div>
+                                        <div className="column">
+                                            <div className='is-flex' style={{justifyContent: 'space-between'}}>
+                                                <Subtitle title={post.category} style={{fontSize: '.8rem', background: 'blue', padding: '.25em'}}/>
+                                                <Subtitle title={new Date(post.datePosted).toDateString()} style={{fontSize: '.8rem'}}/>
+                                            </div>
+                                            <div>
+                                                <Title title={post.title} size='6' />
+                                            </div>
+                                            <div className='is-flex' style={{alignItems: 'center', justifyContent:'flex-start', paddingTop: '.5em'}}>
+                                                <figure class="image is-48x48">
+                                                    <img src={post.authorImg} alt="Placeholder" className='is-rounded'/>
+                                                </figure>
+                                                <Subtitle title={post.author} className='pl-2'/>
+                                            </div>
+                                        </div>
                                     </div>
-                                    <div>
-                                        <Title title='BRRRR Math: A Numeric Breakdown of Buy, Rehab, Rent, Refinance, Repeat' size='6' />
-                                    </div>
-                                    <div className='is-flex' style={{alignItems: 'center', justifyContent:'flex-start', padding: '.5em 0'}}>
-                                        <figure class="image is-48x48">
-                                            <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder" className='is-rounded'/>
-                                        </figure>
-                                        <Subtitle title='Author Name' className='pl-2'/>
-                                    </div>
-                                </div>
-                            </div>
-                        </article>
+                                </article>
+                                )
+                            })}
                         {/* END OF MAP COMPONENT */}
-                        <article class="tile is-child box notification is-warning">
-                            <p class="title">...tiles</p>
-                            <p class="subtitle">Bottom tile</p>
-                        </article>
-                        <article class="tile is-child box notification is-danger">
-                            <p class="title">...tiles</p>
-                            <p class="subtitle">Bottom tile</p>
-                        </article>
                     </div>
                     </div>
                 </div>
